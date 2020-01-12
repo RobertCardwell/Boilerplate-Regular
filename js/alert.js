d7f829100d92710a0bb4fb210c22c0220ea2078c
Robert Cardwell <info@okscomputersupport.com>
2020-01-12 06:50:40 +0000

    added js directory

---
+++ js/alert.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-alert, no-undef
+alert('Hello World!');
+
+// This file is a place-holder and the popup cannot be styled.
+// Better solutions exist for popups, such as jquery.
